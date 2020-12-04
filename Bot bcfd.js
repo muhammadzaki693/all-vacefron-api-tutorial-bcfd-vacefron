41943bfd1984a003f1370c04e9a69f9f7ca294fc
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2020-12-04 12:29:08 +0700

    Create Bot bcfd.js

---
+++ Bot bcfd.js
@@ -0,0 +1,22 @@
+{
+  "Links": [
+  //you need to type + to space
+    "Copy: vacefron.nl/api/carreverse?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/changemymind?text=", 
+    "Copy: vacefron.nl/api/distractedbf?boyfriend=$avatar&woman=$mentionedAvatar&girlfriend=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/ejected?name=$messageAfterCommand&impostor=BOOL&crewmate=$random{black|blue|brown|cyan|darkgreen|lime|orange|pink|purple|red|white|yellow}", 
+    "Copy: vacefron.nl/api/emergencymeeting?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/firsttime?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/grave?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/iamspeed?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/icanmilkyou?user1=$avatar&user2=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/heaven?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/npc?text1=$messageAfterCommand&text2=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/rankcard?username=$messageAfterCommand&avatar=$mentionedAvatar&level=INT&rank=INT&currentxp=INT&nextlevelxp=INT&previouslevelxp=INT[&custombg=$avatar|HEX][&xpcolor=HEX][&isboosting=BOOL]", 
+    "Copy: vacefron.nl/api/stonks?user=$mentionedAvatar[&notstonks=BOOL]", 
+    "Copy: vacefron.nl/api/tableflip?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/water?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/wide?image=$avatar"
+  ], 
+  //you can change $avatar > $mentionedAvatar
+}
