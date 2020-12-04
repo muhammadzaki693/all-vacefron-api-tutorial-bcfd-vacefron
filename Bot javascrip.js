3e1de46a6568fa7d4668dbd7383abff4b1a90470
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2020-12-04 12:12:50 +0700

    Create Bot javascrip.js

---
+++ Bot javascrip.js
@@ -0,0 +1,38 @@
+{
+  "endpoints": [
+    "GET: /carreverse?text=TEXT", 
+    "GET: /changemymind?text=TEXT", 
+    "GET: /distractedbf?boyfriend=USER_AVATAR&woman=USER_AVATAR&girlfriend=USER_AVATAR", 
+    "GET: /ejected?name=TEXT&impostor=BOOL&crewmate=black|blue|brown|cyan|darkgreen|lime|orange|pink|purple|red|white|yellow", 
+    "GET: /emergencymeeting?text=TEXT", 
+    "GET: /firsttime?user=USER_AVATAR", 
+    "GET: /grave?user=USER_AVATAR", 
+    "GET: /iamspeed?user=USER_AVATAR", 
+    "GET: /icanmilkyou?user1=USER_AVATAR&user2=USER_AVATAR", 
+    "GET: /heaven?user=USER_AVATAR", 
+    "GET: /npc?text1=TEXT&text2=TEXT", 
+    "GET: /rankcard?username=TEXT&avatar=URL&level=INT&rank=INT&currentxp=INT&nextlevelxp=INT&previouslevelxp=INT[&custombg=URL|HEX][&xpcolor=HEX][&isboosting=BOOL]", 
+    "GET: /stonks?user=USER_AVATAR[&notstonks=BOOL]", 
+    "GET: /tableflip?user=USER_AVATAR", 
+    "GET: /water?text=TEXT", 
+    "GET: /wide?image=IMAGE_URL"
+  ], 
+  "wrappers": {
+    "csharp": {
+      "github": "https://github.com/VACEfron/VACEfron.NET", 
+      "nuget": "https://www.nuget.org/packages/VACEfron.NET"
+    }, 
+    "python": {
+      "github": "https://github.com/Soheab/vacefron.py", 
+      "pypi": "https://pypi.org/project/vacefron.py"
+    }, 
+    "javascript": {
+      "github": "https://github.com/Sidemen19/VACEfron.js", 
+      "npm": "https://npmjs.com/package/vacefron"
+    }, 
+    "java": {
+      "github": "https://github.com/thunderstorm010/VACEfron4J"
+    }
+  }, 
+  "discord_server": "https://discord.gg/xJ2HRxZ"
+}
